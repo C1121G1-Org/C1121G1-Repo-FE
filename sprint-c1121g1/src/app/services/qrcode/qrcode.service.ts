95995e29c8186a97c22f70db979b5a47a5e94dad
haupham96 <hau.pv.0596@gmail.com>
2022-06-02 16:01:36 +0700

    HauPV

--- sprint-c1121g1/src/app/services/qrcode/qrcode.service.ts
+++ sprint-c1121g1/src/app/services/qrcode/qrcode.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class QrcodeService {
+  URL = "http://localhost:8080/api/qrcode";
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  decode(formData: FormData): Observable<any> {
+    return this.http.post<any>(`${this.URL}/decode`, formData);
+  }
+
+  check(formData: FormData): Observable<any> {
+    return this.http.post<any>(`${this.URL}/check`, formData);
+  }
+
 }
