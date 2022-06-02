e58ae4bf3865e4eb1ed59671d13f12973c940b01
haupham96 <hau.pv.0596@gmail.com>
2022-06-02 13:48:59 +0700

    HauPV

--- sprint-c1121g1/src/app/component/report/report.module.ts
+++ sprint-c1121g1/src/app/component/report/report.module.ts
@@ -4,8 +4,9 @@ import { CommonModule } from '@angular/common';
 import { ReportRoutingModule } from './report-routing.module';
 import { SaleReportComponent } from './sale-report/sale-report.component';
 import {HttpClientModule} from "@angular/common/http";
-import {CustomerReportComponent} from './customer-report/customer-report.component';
-import {PurchaseHistoryComponent} from './purchase-history/purchase-history.component';
+import {CustomerReportComponent} from "./customer-report/customer-report.component";
+import {PurchaseHistoryComponent} from "./purchase-history/purchase-history.component";
+
 
 
 
