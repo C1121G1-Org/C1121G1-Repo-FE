7d33aa2608073f845961fe9553dda9d0e957a8dc
haupham96 <hau.pv.0596@gmail.com>
2022-06-02 11:32:08 +0700

    pull dev

--- sprint-c1121g1/src/app/component/report/report.module.ts
+++ sprint-c1121g1/src/app/component/report/report.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ReportRoutingModule } from './report-routing.module';
 import { SaleReportComponent } from './sale-report/sale-report.component';
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
   declarations: [SaleReportComponent],
   imports: [
     CommonModule,
-    ReportRoutingModule
+    ReportRoutingModule,
+    HttpClientModule
   ]
 })
 export class ReportModule { }
