d070ce5ee1730b14b3282e9fb76b69e98eb597ab
haupham96 <hau.pv.0596@gmail.com>
2022-06-03 21:11:03 +0700

    hauPV

--- sprint-c1121g1/src/app/component/shared/qr-code/qr-code.component.ts
+++ sprint-c1121g1/src/app/component/shared/qr-code/qr-code.component.ts
@@ -24,6 +24,7 @@ export class QrCodeComponent implements OnInit {
   product: Product = {};
 
   message = '';
+  alertClass = '';
   typeQRScan = '1';
 
   image1 = 'https://uniquartz.co.nz/wp-content/uploads/2018/06/image_large.png';
@@ -39,7 +40,7 @@ export class QrCodeComponent implements OnInit {
   scanQRCode() {
 
     const file: any = document.querySelectorAll('input[type=\'file\']');
-    // tslint:disable-next-line:triple-equals
+
     if (this.typeQRScan == '1') {
       if (file[0].files[0]) {
         const formData = new FormData();
@@ -51,23 +52,37 @@ export class QrCodeComponent implements OnInit {
           console.log(err);
         });
         this.message = '';
+        this.alertClass = '';
       } else {
+        this.alertClass = 'alert alert-danger';
         this.message = 'Vui lòng chọn ảnh !';
         this.image1 = 'https://uniquartz.co.nz/wp-content/uploads/2018/06/image_large.png';
       }
 
     } else {
       if (file[0].files[0] && file[1].files[0]) {
+        this.alertClass = '';
+        this.message = '';
         const formData = new FormData();
         formData.append('file1', file[0].files[0]);
         formData.append('file2', file[1].files[0]);
         this.qrCodeService.check(formData).subscribe(data => {
-          console.log(data);
+          if (data) {
+            this.alertClass = 'alert alert-success';
+            this.message = 'Mã QR HỢP LỆ !';
+          } else {
+            this.alertClass = 'alert alert-danger';
+            this.message = 'Mã QR KHÔNG HỢP LỆ !';
+          }
+
         }, err => {
-          console.log(err);
+          this.alertClass = 'alert alert-danger';
+          this.message = 'Mã QR KHÔNG HỢP LỆ !';
         });
+        this.alertClass = '';
         this.message = '';
       } else {
+        this.alertClass = 'alert alert-danger';
         this.message = 'Vui lòng chọn ảnh !';
       }
     }
@@ -83,6 +98,7 @@ export class QrCodeComponent implements OnInit {
       };
       this.message = '';
     } else {
+      this.alertClass = 'alert alert-danger';
       this.message = 'Vui lòng chọn ảnh !';
       this.image1 = 'https://uniquartz.co.nz/wp-content/uploads/2018/06/image_large.png';
     }
@@ -98,7 +114,9 @@ export class QrCodeComponent implements OnInit {
         this.image2 = reader?.result as string;
       };
       this.message = '';
+      this.alertClass = '';
     } else {
+      this.alertClass = 'alert alert-danger';
       this.message = 'Vui lòng chọn ảnh !';
       this.image2 = 'https://uniquartz.co.nz/wp-content/uploads/2018/06/image_large.png';
     }
--- sprint-c1121g1/src/app/services/report/sale-report.service.ts
+++ sprint-c1121g1/src/app/services/report/sale-report.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
@@ -12,7 +12,7 @@ export class SaleReportService {
   constructor(private http: HttpClient) {
   }
 
-  getAllSaleREports():Observable<any>{
-    return this.http.get<any>(`${this.URL_SALE_REPORT}`);
+  getAllSaleREports(startDay: string, endDay: string, productId: string): Observable<any> {
+    return this.http.get<any>(`${this.URL_SALE_REPORT}?startDay=${startDay}&endDay=${endDay}&productId=${productId}`);
   }
 }
