ef408a0af90be4da59cb6794cc32e4594c75e0bf
haupham96 <hau.pv.0596@gmail.com>
2022-06-06 10:15:33 +0700

    merge dev

--- sprint-c1121g1/src/app/services/product/product.service.ts
+++ sprint-c1121g1/src/app/services/product/product.service.ts
@@ -33,31 +33,29 @@ export class ProductService {
     return this.http.post<any>(this.productApi + '/create', product);
   }
 
-<<<<<<< HEAD
-=======
+
   /*
     Created by TuanPA
     Date: 9:08 3/6/2022
     Function: find by id
 */
->>>>>>> f1b918f96a955034e9887d1cb36d548fb8e7dc60
+
   findById(id: number): Observable<Product> {
     return this.http.get<Product>(this.productApi + '/' + id);
   }
 
-<<<<<<< HEAD
-=======
+
   /*
     Created by TuanPA
     Date: 9:08 3/6/2022
     Function: edit product
 */
->>>>>>> f1b918f96a955034e9887d1cb36d548fb8e7dc60
+
   updateProduct(id: number, value: any): Observable<any> {
     return this.http.patch<any>(this.productApi + '/' + 'update/' + id, value);
   }
 
-<<<<<<< HEAD
+
   listAll(): Observable<any> {
     return this.http.get<any>(`${this.apiBaseUrl}/api/product/list`);
   }
@@ -69,9 +67,6 @@ export class ProductService {
 */
   list(): Observable<any> {
     return this.http.get<any>(`${this.apiBaseUrl}/api/product/listAll`);
-=======
-  list(): Observable<Product[]> {
-    return this.http.get<Product[]>(`${this.apiBaseUrl}/api/product/list`);
->>>>>>> f1b918f96a955034e9887d1cb36d548fb8e7dc60
   }
+
 }
--- sprint-c1121g1/src/app/services/report/report-and-history.service.ts
+++ sprint-c1121g1/src/app/services/report/report-and-history.service.ts
@@ -29,15 +29,7 @@ export class ReportAndHistoryService {
       'http://localhost:8080/api/customer/info-customer/' + id);
   }
 
-<<<<<<< HEAD
-  getPurchaseHistory(id: number, page: number): Observable<any> {
-    return this.httpClient.get<PurchaseHistoryDto[]>(
-      'http://localhost:8080/api/customer/purchase-history/' + id + '?page=' + page);
-  }
 
-  getPurchaseProduct(id: number, page: number): Observable<any> {
-    return this.httpClient.get<PurchaseProductDto[]>(
-=======
   getPurchaseHistory(id: number, startDate: string, endDate: string, page: number):
     Observable<any> {
     return this.httpClient.get<any>(
@@ -46,9 +38,7 @@ export class ReportAndHistoryService {
   }
 
   getPurchaseProduct(id: number, page: number): Observable<any> {
-    return this.httpClient.get<any>(
->>>>>>> b8ad0b23e539e49c7fb9982929b12b619e2ee5fb
-      'http://localhost:8080/api/customer/purchase-products/' + id + '?page=' + page);
+    return this.httpClient.get<any>('http://localhost:8080/api/customer/purchase-products/' + id + '?page=' + page);
   }
 
   filterByGender(page: number, genderSearch: boolean):
