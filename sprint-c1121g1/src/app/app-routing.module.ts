de5ca55de0bb9636033f89a7c15799ec63bb5897
TrungHieu175 <ablohaha01@gmail.com>
2022-06-08 10:25:03 +0700

    up manager by hieu MM

--- sprint-c1121g1/src/app/app-routing.module.ts
+++ sprint-c1121g1/src/app/app-routing.module.ts
@@ -2,22 +2,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {ProductListBestsellerComponent} from './component/homepage/product-list-bestseller/product-list-bestseller.component';
-<<<<<<< HEAD
-import {StorageCreateComponent} from './component/storage/storage-create/storage-create.component';
-
-import {ProductCreateComponent} from './component/product/product-create/product-create.component';
 import {ReportModule} from './component/report/report.module';
-import {ProductUpdateComponent} from './component/product/product-update/product-update.component';
 import {ProductListComponent} from "./component/product/product-list/product-list.component";
-=======
-import {QrcodeComponent} from './common/qrcode/qrcode.component';
-
-
 import {ProductCreateComponent} from './component/product/product-create/product-create.component';
-
 import {StorageCreateComponent} from './component/storage/storage-create/storage-create.component';
 import {ProductUpdateComponent} from './component/product/product-update/product-update.component';
->>>>>>> manager-product
 
 
 
@@ -25,7 +14,6 @@ const routes: Routes = [
   {path: '', component: ProductListBestsellerComponent},
   {path: 'api/product/create', component: ProductCreateComponent},
   {path: 'api/product/edit/:id', component: ProductUpdateComponent},
-<<<<<<< HEAD
   {path: 'storage/create', component: StorageCreateComponent},
   {path: 'storage/create', component: StorageCreateComponent},
   {path: 'api/product/edit/:id', component: ProductUpdateComponent},
@@ -33,10 +21,6 @@ const routes: Routes = [
   {path:'listProduct',component: ProductListComponent},
   {path: 'product/create',component : ProductCreateComponent},
   {path: 'product/edit/:id',component : ProductUpdateComponent},
-=======
-
-  {path: 'storage/create', component: StorageCreateComponent}
->>>>>>> manager-product
 
 ];
 
